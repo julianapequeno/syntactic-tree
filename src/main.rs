bf3b824ae2f08da4865533dacd04ab99edd01f09
julianapequeno <julianafpsc@gmail.com>
2024-05-07 10:36:29 -0300

    feat: imprimir_expr

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ enum Expressao {
     Numero(i64),
     OperadorBinario {
         operador: Operador,
-        esquerda: Box<Expressao>,
+        esquerda: Box<Expressao>, //Box faz a var não ser copiável, é uma alocacao de memória
         direita: Box<Expressao>,
     },
     OperadorUnario(Box<Expressao>),
@@ -18,7 +18,7 @@ enum Expressao {
 impl Expressao {
     fn avaliar(&self) -> Option<i64> {
         match self {
-            Expressao::Numero(i) => Some(*i),
+            Expressao::Numero(i) => Some(*i), //&i64
             Expressao::OperadorUnario(e) => e.avaliar()?.checked_neg(),
             Expressao::OperadorBinario { operador, esquerda, direita} => {
                 let e = esquerda.avaliar()?;
@@ -54,7 +54,30 @@ impl Expressao {
         }
     }
 
+    fn imprimir_expressao(&self){
+        match self { //match na referencia, permite usar a func varias vezes sem passar a ownership para ela e bloquear o seu us
+            Expressao::Numero(i) => print!("{}",*i),
+            Expressao::OperadorUnario(e) => {
+                print!("-");
+                let e: &Expressao = &e; //Para resolver o caso de esperar um Box<Ex..>
+                //o match não aceitou o deref() pq não sabia o tipo que o user queria
+                //deref() converte ref do box para uma ref expressao
+                match e {
+                    Expressao::Numero(_) | Expressao::OperadorUnario(_) => e.imprimir_expressao(),
+                    _ => {
+                        print!("{{");
+                        e.imprimir_expressao();
+                        print!("}}");
+                    }
+                }
+            }
+            _ => todo!()
+        }
+    }
+
     fn imprimir(&self) {
+        self.imprimir_expressao();
+        println!("");
     }
 
     fn imprimir_arvore(&self) {
@@ -64,8 +87,10 @@ impl Expressao {
 
 
 fn main() {
-   let n10 = Expressao::Numero(10); // i64::MAX
+    let n10 = Expressao::Numero(10); // i64::MAX
+    n10.imprimir();
     let n20 = Expressao::Numero(20); //i64::MIN + 1
+    /*
     let e = Expressao::OperadorBinario {
         operador: Operador::Subtracao,
         esquerda: Box::new(Expressao::Numero(50)),
@@ -76,9 +101,10 @@ fn main() {
         operador: Operador::Adicao,
         esquerda: Box::new(n10),
         direita: Box::new(e),
-    };
+    };*/
 
-    let e = Expressao::OperadorUnario(Box::new(e));
+    let e = Expressao::OperadorUnario(Box::new(Expressao::OperadorUnario(Box::new(n20)))); //e
+    e.imprimir();
     e.imprimir_arvore();
     println!("{e:?}");
     println!("Resultado: {:?}",e.avaliar());
